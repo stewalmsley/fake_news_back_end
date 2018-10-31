47ba0fa44276a476e51ead0114ead161a3c1c060
Stephen Walmsley <stewalmsley@Stephens-MacBook-Pro.local>
2018-10-31 14:35:26 +0000

    access control allow methods

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.use(bodyParser.json()),
 app.use(express.static('public'))
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Methods: POST, GET, PATCH, DELETE", "Origin, X-Requested-With, Content-Type, Accept");
+    
     next();
   });
 app.get('/', (req, res, next) => res.sendFile('index.html'))
