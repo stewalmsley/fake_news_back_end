2eae2ef42cc2ebe8d71cccb1a8dad860f7ef7e4b
Stephen Walmsley <stewalmsley@Stephens-MacBook-Pro.local>
2018-11-10 14:30:13 +0000

    adjusting vote and comment count key name

--- utils/commentcount.js
+++ utils/commentcount.js
@@ -25,11 +25,11 @@ exports.addAuthorsAndTopics = (articles) => {
     const topics = []
     for (let i = 0; i < articles.length; i++) {
         const authorIndex = authors.findIndex(author => author._id == articles[i].created_by._id)
-        if (authorIndex === -1) authors.push({...articles[i].created_by, articleCount: 1, receivedCommentCount: articles[i].commentCount, receivedVotes: articles[i].votes})
+        if (authorIndex === -1) authors.push({...articles[i].created_by, articleCount: 1, commentCount: articles[i].commentCount, votes: articles[i].votes})
         else {
             authors[authorIndex].articleCount ++
-            authors[authorIndex].receivedCommentCount += articles[i].commentCount
-            authors[authorIndex].receivedVotes += articles[i].votes;
+            authors[authorIndex].commentCount += articles[i].commentCount
+            authors[authorIndex].votes += articles[i].votes;
         }
         const topicIndex = topics.findIndex(topic => topic.slug == articles[i].belongs_to)
         if (topicIndex === -1) topics.push({ slug: articles[i].belongs_to, articleCount: 1, commentCount: articles[i].commentCount, votes: articles[i].votes})
